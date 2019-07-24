ddea647ea2b69303b244f712dc1dd8d9a474fd92
KF4RD <kalewford@outlook.com>
2019-07-24 18:27:44 -0500

    added php file for future use

--- handler.php
+++ handler.php
@@ -1,28 +1,15 @@
 <?php
+
+if (isset($_POST['submit'])) {
 $name = $_POST['name'];
-$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$mailFrom = $_POST['mail'];
 $message = $_POST['message'];
 
+$mailTo = "kalewford@outlook.com";
+$headers = "From: ".$mailFrom;
+$txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
-$email_from = 'kalewford@outlook.com';
-
-$email_subject = "New Form Submission";
-
-$email_body = "User Name: $name.\n".
-"User Email: $visitor_email.\n".
-"User Message: $message.\n";
-
-
-$to = "kalewford@outlook.com";
-
-$headers = "From: $email_from \r\n";
-
-$headers .= "Reply To: $visitor_email \r\n";
-
-mail($to, email_subject, $email_body, $headers);
-
-header("Location: Website Name");
-
-
-
-?>
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.php?mailsend");
+}
